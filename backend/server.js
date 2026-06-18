48a5692a6afb407d54fffc1bfb7309022e5162a2
swastikanuragvyas <swastikanuragvyas@gmail.com>
2026-06-18 13:25:23 +0530

    ee

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.use("/api/contact", require("./routes/contact"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.use("/api/contact", require("./routes/contact"));
