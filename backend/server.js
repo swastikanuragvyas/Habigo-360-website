c315f6d6226efde88de0375ce4c42437cfac5409
swastikanuragvyas <swastikanuragvyas@gmail.com>
2026-06-24 16:29:19 +0530

    photos

--- backend/server.js
+++ backend/server.js
@@ -34,4 +34,10 @@ const PORT = process.env.PORT || 5000;
 // Start Cron Jobs
 require("./cron/dailySync")();
 
+// Global Error Handler
+app.use((err, req, res, next) => {
+  console.error("Express Error:", err.message, err);
+  res.status(500).json({ message: err.message || "Internal Server Error" });
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
