7d1a4779efa6e2d22e3700423b7b5527e8f7c2c0
swastikanuragvyas <swastikanuragvyas@gmail.com>
2026-07-09 17:31:53 +0530

    push

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   nitro: {
-    preset: "vercel" // <-- Explicitly set to Vercel
+    preset: "static",
   },
   tanstackStart: {
     server: { entry: "server" },
